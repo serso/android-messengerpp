ad124ad5a13265f2b93cf808ea78fe62d2e34fa3
Sergey Solovyev <se.solovyev@gmail.com>
2013-02-24 00:50:16 +0400

    changes

---
+++ core/src/main/java/org/solovyev/android/messenger/FooterButtonBuilder.java
@@ -0,0 +1,39 @@
+package org.solovyev.android.messenger;
+
+import android.content.Context;
+import android.widget.Button;
+import org.jetbrains.annotations.NotNull;
+import org.solovyev.android.view.ViewBuilder;
+import org.solovyev.android.view.ViewFromLayoutBuilder;
+
+/**
+ * User: serso
+ * Date: 4/28/12
+ * Time: 7:57 PM
+ */
+public class FooterButtonBuilder implements ViewBuilder<Button> {
+
+    private int captionResId;
+
+    private FooterButtonBuilder() {
+    }
+
+    @NotNull
+    public static ViewBuilder<Button> newInstance(int captionResId) {
+        final FooterButtonBuilder result = new FooterButtonBuilder();
+
+        result.captionResId = captionResId;
+
+        return result;
+    }
+
+    @NotNull
+    @Override
+    public Button build(@NotNull Context context) {
+        final Button result = ViewFromLayoutBuilder.<Button>newInstance(R.layout.footer_button).build(context);
+
+        result.setText(context.getText(captionResId));
+
+        return result;
+    }
+}
---
+++ core/src/main/java/org/solovyev/android/messenger/FooterImageButtonBuilder.java
@@ -0,0 +1,44 @@
+package org.solovyev.android.messenger;
+
+import android.content.Context;
+import android.widget.ImageButton;
+import org.jetbrains.annotations.NotNull;
+import org.solovyev.android.view.ViewBuilder;
+import org.solovyev.android.view.ViewFromLayoutBuilder;
+
+/**
+ * User: serso
+ * Date: 4/28/12
+ * Time: 7:50 PM
+ */
+public class FooterImageButtonBuilder implements ViewBuilder<ImageButton> {
+
+    private int imageResId;
+
+    private int contentDescriptionResId;
+
+    private FooterImageButtonBuilder() {
+    }
+
+    @NotNull
+    public static ViewBuilder<ImageButton> newInstance(int imageResId, int contentDescriptionResId) {
+        final FooterImageButtonBuilder result = new FooterImageButtonBuilder();
+
+        result.imageResId = imageResId;
+        result.contentDescriptionResId = contentDescriptionResId;
+
+        return result;
+    }
+
+    @NotNull
+    @Override
+    public ImageButton build(@NotNull Context context) {
+        final ImageButton result = ViewFromLayoutBuilder.<ImageButton>newInstance(R.layout.footer_image_button).build(context);
+
+        result.setImageDrawable(context.getResources().getDrawable(imageResId));
+        result.setContentDescription(context.getString(contentDescriptionResId));
+
+        return result;
+    }
+}
+
