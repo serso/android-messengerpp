4f9c7693dedbd1dba297b4cfc72eb131c52680fc
Sergey Solovyev <se.solovyev@gmail.com>
2013-10-07 21:25:43 +0400

    tests added

--- realm-xmpp/src/main/java/org/solovyev/android/messenger/realms/xmpp/XmppChatListener.java
+++ realm-xmpp/src/main/java/org/solovyev/android/messenger/realms/xmpp/XmppChatListener.java
@@ -1,11 +1,6 @@
 package org.solovyev.android.messenger.realms.xmpp;
 
 import android.util.Log;
-
-import java.util.Collections;
-
-import javax.annotation.Nonnull;
-
 import org.jivesoftware.smack.ChatManagerListener;
 import org.jivesoftware.smack.packet.Message;
 import org.solovyev.android.messenger.App;
@@ -14,6 +9,11 @@ import org.solovyev.android.messenger.accounts.AccountException;
 import org.solovyev.android.messenger.chats.ApiChat;
 import org.solovyev.android.messenger.chats.Chat;
 import org.solovyev.android.messenger.chats.ChatService;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+
+import static org.solovyev.android.messenger.realms.xmpp.XmppRealm.TAG;
 
 final class XmppChatListener implements ChatManagerListener {
 
@@ -26,13 +26,17 @@ final class XmppChatListener implements ChatManagerListener {
 
 	@Override
 	public void chatCreated(@Nonnull org.jivesoftware.smack.Chat chat, boolean createdLocally) {
-		Log.i("M++/Xmpp", "Chat created!");
+		Log.d(TAG, "Chat created!");
 
 		if (!createdLocally) {
 			final ApiChat newApiChat = XmppAccount.toApiChat(chat, Collections.<Message>emptyList(), account);
 			try {
 				final Chat newChat = getChatService().saveChat(account.getUser().getEntity(), newApiChat);
-				chat.addMessageListener(new XmppMessageListener(account, newChat.getEntity()));
+				if (newChat != null) {
+					chat.addMessageListener(new XmppMessageListener(account, newChat.getEntity()));
+				} else {
+					Log.e(TAG, "Chat has not been found for thread id: " + chat.getThreadID());
+				}
 			} catch (AccountException e) {
 				App.getExceptionHandler().handleException(e);
 			}
