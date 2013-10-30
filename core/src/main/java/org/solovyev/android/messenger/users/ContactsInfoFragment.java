71e05b7d657f4829b182b3446105a9bcc73a9dce
Sergey Solovyev <se.solovyev@gmail.com>
2013-10-30 17:35:52 +0400

    contacts info

--- core/src/main/java/org/solovyev/android/messenger/users/ContactsInfoFragment.java
+++ core/src/main/java/org/solovyev/android/messenger/users/ContactsInfoFragment.java
@@ -20,18 +20,15 @@ import org.solovyev.android.messenger.App;
 import org.solovyev.android.messenger.MultiPaneManager;
 import org.solovyev.android.messenger.core.R;
 import org.solovyev.android.messenger.entities.Entities;
-import org.solovyev.android.messenger.realms.Realm;
 import org.solovyev.android.view.ViewFromLayoutBuilder;
 
 import com.github.rtyley.android.sherlock.roboguice.fragment.RoboSherlockFragment;
-import com.google.common.base.Splitter;
+import com.google.common.base.Function;
 import com.google.inject.Inject;
 
-/**
- * User: serso
- * Date: 8/19/12
- * Time: 5:52 PM
- */
+import static com.google.common.collect.Lists.transform;
+import static java.util.Arrays.asList;
+
 public class ContactsInfoFragment extends RoboSherlockFragment {
 
 	@Nonnull
@@ -45,48 +42,39 @@ public class ContactsInfoFragment extends RoboSherlockFragment {
 	@Nonnull
 	private MultiPaneManager multiPaneManager;
 
-	@Inject
 	@Nonnull
-	private Realm realm;
-
-	@Nonnull
-	private static final String CONTACT_IDS = "contact_ids";
+	private static final String ARG_CONTACT_IDS = "contact_ids";
 
 	private List<User> contacts;
+
 	private Iterable<String> contactIds;
 
 	public ContactsInfoFragment() {
 	}
 
-	public ContactsInfoFragment(@Nonnull Iterable<String> contactIds) {
-		this.contactIds = contactIds;
-	}
-
 	public ContactsInfoFragment(@Nonnull List<User> contacts) {
 		this.contacts = contacts;
+		final Bundle arguments = new Bundle();
+		arguments.putStringArray(ARG_CONTACT_IDS, transform(contacts, new Function<User, String>() {
+			@Override
+			public String apply(User user) {
+				return user.getId();
+			}
+		}).toArray(new String[contacts.size()]));
+		setArguments(arguments);
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		final View result = ViewFromLayoutBuilder.newInstance(R.layout.mpp_contacts).build(this.getActivity());
+		final View result = ViewFromLayoutBuilder.newInstance(R.layout.mpp_fragment_contacts).build(this.getActivity());
 
 		multiPaneManager.onCreatePane(this.getActivity(), container, result);
 
-		result.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-
-		return result;
-	}
-
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-
-		// restore state
 		if (contacts == null) {
-			if (contactIds == null) {
-				final String contactIdsString = savedInstanceState.getString(CONTACT_IDS);
-				if (contactIdsString != null) {
-					contactIds = Splitter.on(';').split(contactIdsString);
+			if (this.contactIds == null) {
+				final String[] contactIds = getArguments().getStringArray(ARG_CONTACT_IDS);
+				if (contactIds != null) {
+					this.contactIds = asList(contactIds);
 				}
 			}
 
@@ -103,18 +91,25 @@ public class ContactsInfoFragment extends RoboSherlockFragment {
 			}
 		}
 
-		final ViewGroup root = (ViewGroup) getView().findViewById(R.id.contacts_container);
+		return result;
+	}
+
+	@Override
+	public void onViewCreated(View root, Bundle savedInstanceState) {
+		super.onViewCreated(root, savedInstanceState);
+
+		final ViewGroup contactsView = (ViewGroup) root.findViewById(R.id.contacts_container);
 
 		final boolean portrait = Views.getScreenOrientation(this.getActivity()) == Configuration.ORIENTATION_PORTRAIT;
 
 		ViewGroup contactsRow = null;
-		for (int i = 0; i < contacts.size(); i++) {
-			final User contact = contacts.get(i);
+		for (int i = 0; i < this.contacts.size(); i++) {
+			final User contact = this.contacts.get(i);
 
 			final LinearLayout contactContainer;
 			if (i % 2 == 0) {
-				contactsRow = ViewFromLayoutBuilder.<ViewGroup>newInstance(R.layout.mpp_contacts_row).build(this.getActivity());
-				root.addView(contactsRow);
+				contactsRow = ViewFromLayoutBuilder.<ViewGroup>newInstance(R.layout.mpp_fragment_contacts_row).build(this.getActivity());
+				contactsView.addView(contactsRow);
 				contactContainer = (LinearLayout) contactsRow.findViewById(R.id.left_contact_container);
 			} else {
 				contactContainer = (LinearLayout) contactsRow.findViewById(R.id.right_contact_container);
@@ -132,17 +127,8 @@ public class ContactsInfoFragment extends RoboSherlockFragment {
 			final ImageView contactIcon = (ImageView) contactContainer.findViewById(R.id.mpp_contact_icon_imageview);
 			App.getUserService().getIconsService().setUserPhoto(contact, contactIcon);
 		}
-	}
 
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-
-		final StringBuilder sb = new StringBuilder();
-		for (User contact : contacts) {
-			sb.append(contact.getEntity().getEntityId()).append(";");
-		}
-		outState.putString(CONTACT_IDS, sb.toString());
+		multiPaneManager.onPaneCreated(this.getActivity(), root);
 	}
 }
 
