308be18fe559f37d0c5ccf6abe12cf9324f9ff5d
serso <se.solovyev@gmail.com>
2013-12-03 00:43:37 +0400

    regression tests added

--- core/misc/bin/config.py
+++ core/misc/bin/config.py
@@ -1,5 +1,56 @@
-def get_activities():
-    return ['.StartActivity', '.preferences.PreferencesActivity', '.accounts.AccountsActivity', '.about.AboutActivity']
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+from com.android.monkeyrunner.easy import EasyMonkeyDevice
+from com.android.monkeyrunner.easy import By
+import time
+
+
+def get_actions(device_name):
+    tests = {'startMainActivity': lambda device: start_activity(device, '.StartActivity'),
+             'startPreferencesActivity': lambda device: start_activity(device, '.preferences.PreferencesActivity'),
+             'startAccountsActivity': lambda device: start_activity(device, '.accounts.AccountsActivity'),
+             'startAboutActivity': lambda device: start_activity(device, '.about.AboutActivity'),
+             'openMenu': open_menu
+    }
+
+    for name, action in get_device_actions().get(device_name, {}).iteritems():
+        tests[name] = action
+
+    return tests
+
+
+def get_device_actions():
+    return {'Nexus-4': {
+        'verticalScrollInMainActivity': vertical_scroll
+    }}
+
+
+def open_menu(device):
+    start_activity(device, '.StartActivity')
+    device.press('KEYCODE_MENU', "DOWN_AND_UP")
+
+
+def vertical_scroll(device):
+    start_activity(device, '.StartActivity')
+
+    width = int(device.getProperty('display.width'))
+    height = int(device.getProperty('display.height'))
+    device.drag((width / 2, height / 2 + height / 3), (width / 2, height / 2), 0.2, 1)
+
+    time.sleep(1)
+
+
+def open_contacts_filter(device):
+    easy_device = EasyMonkeyDevice(device)
+    start_activity(device, '.StartActivity')
+
+    easy_device.touch(By.id('id/mpp_menu_toggle_filter_box'), MonkeyDevice.DOWN_AND_UP)
+    time.sleep(1)
+
+
+def start_activity(device, activity):
+    run_component = get_package() + '/' + activity
+    device.startActivity(run_component)
+    time.sleep(5)
 
 
 def get_package():
--- core/misc/bin/take_screenshots.sh
+++ core/misc/bin/take_screenshots.sh
@@ -2,12 +2,12 @@
 
 scale=0.51
 
-names=(Default Nexus-4 Nexus-7)
+names=(Nexus-4 Nexus-7)
 for name in ${names[*]}
 do
      $ANDROID_HOME/tools/emulator -ports 5580,5581 -avd $name -scale $scale &
      sleep 10
-     $ANDROID_HOME/tools/monkeyrunner ./take_screenshots.py ../screenshots-new $name android-messenger-app.apk
+     $ANDROID_HOME/tools/monkeyrunner ./take_screenshots.py ../screenshots-new $name ../../../app/target/android-messenger-app.apk
      $ANDROID_HOME/platform-tools/adb -s emulator-5580 emu kill
      sleep 3
 done
