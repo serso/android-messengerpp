b789bd56b6a609fa9d76f5e9e8ba107c35a6796f
serso <se.solovyev@gmail.com>
2013-12-07 22:56:55 +0400

    apk_location parameter for take_screenshot.sh

--- core/misc/bin/take_screenshots.sh
+++ core/misc/bin/take_screenshots.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apk_file=$1
+
 scale=0.51
 
 names=(Android-2.3 Nexus-4 Nexus-7)
@@ -7,7 +9,7 @@ for name in ${names[*]}
 do
      $ANDROID_HOME/tools/emulator -ports 5580,5581 -avd $name -scale $scale &
      sleep 10
-     $ANDROID_HOME/tools/monkeyrunner ./take_screenshots.py ../screenshots-new $name ../../../app/target/android-messenger-app.apk
+     $ANDROID_HOME/tools/monkeyrunner ./take_screenshots.py ../screenshots-new $name $apk_file
      $ANDROID_HOME/platform-tools/adb -s emulator-5580 emu kill
      sleep 3
 done
