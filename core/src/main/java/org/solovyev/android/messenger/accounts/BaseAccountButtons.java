fee21e3c609f1bc08e448b901f0fa0f38e938a63
Sergey Solovyev <se.solovyev@gmail.com>
2013-12-09 18:34:20 +0400

    account removal confirmation

--- core/src/main/java/org/solovyev/android/messenger/accounts/BaseAccountButtons.java
+++ core/src/main/java/org/solovyev/android/messenger/accounts/BaseAccountButtons.java
@@ -1,10 +1,13 @@
 package org.solovyev.android.messenger.accounts;
 
+import android.content.DialogInterface;
+
 import javax.annotation.Nonnull;
 
 import org.solovyev.android.messenger.EditButtons;
 import org.solovyev.android.messenger.accounts.tasks.AccountRemoverCallable;
 import org.solovyev.android.messenger.core.R;
+import org.solovyev.android.view.ConfirmationDialogBuilder;
 
 import static org.solovyev.android.messenger.accounts.tasks.AccountRemoverListener.newAccountRemoverListener;
 
@@ -21,8 +24,15 @@ public abstract class BaseAccountButtons<A extends Account<?>, F extends BaseAcc
 
 	@Override
 	protected void onRemoveButtonPressed() {
-		final F fragment = getFragment();
-		fragment.getTaskListeners().run(AccountRemoverCallable.TASK_NAME, new AccountRemoverCallable(getAccount()), newAccountRemoverListener(getActivity()), getActivity(), R.string.mpp_removing_account_title, R.string.mpp_removing_account_message);
+		final F f = getFragment();
+		final ConfirmationDialogBuilder builder = ConfirmationDialogBuilder.newInstance(f.getFragmentActivity(), "account-removal-confirmation", R.string.mpp_account_removal_confirmation);
+		builder.setPositiveHandler(new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				f.getTaskListeners().run(AccountRemoverCallable.TASK_NAME, new AccountRemoverCallable(getAccount()), newAccountRemoverListener(getActivity()), getActivity(), R.string.mpp_removing_account_title, R.string.mpp_removing_account_message);
+			}
+		});
+		builder.build().show();
 	}
 
 	@Nonnull
