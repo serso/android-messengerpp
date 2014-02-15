d7e36dd7be85700967a9a51034f2222f9adf2321
serso <se.solovyev@gmail.com>
2014-02-15 14:46:24 +0100

    Unread messages notification is not updated when message is marked read

--- core/src/main/java/org/solovyev/android/messenger/UnreadMessagesNotifier.java
+++ core/src/main/java/org/solovyev/android/messenger/UnreadMessagesNotifier.java
@@ -62,26 +62,30 @@ public final class UnreadMessagesNotifier extends AbstractJEventListener<Messeng
 			case unread_messages_count_changed:
 				final Integer unreadMessagesCount = event.getDataAsInteger();
 
-				if (unreadMessagesCount == 0) {
-					final NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-					nm.cancel(NOTIFICATION_ID_UNREAD_MESSAGES);
-				} else {
-					final ActivityManager.RunningTaskInfo foregroundTask = getForegroundTask();
-					if (foregroundTask == null || !foregroundTask.topActivity.getPackageName().equals(context.getPackageName())) {
-						// todo serso: make proper notification (unread messages text, small icon, etc)
-						// we are not at the top => show notification
-						final NotificationCompat.Builder nb = new NotificationCompat.Builder(context);
-						nb.setSmallIcon(R.drawable.mpp_sb_unread_messages_icon);
-						nb.setContentTitle(context.getResources().getQuantityString(R.plurals.mpp_unread_messages_count_notification, unreadMessagesCount, unreadMessagesCount));
-						nb.setContentText(context.getString(R.string.mpp_notification_text));
-						nb.setContentIntent(getActivity(context, 0, newUnreadMessagesStartIntent(context), 0));
-						nb.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
-						final NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-						nm.notify(NOTIFICATION_ID_UNREAD_MESSAGES, nb.getNotification());
-					}
+				final NotificationManager nm = getNotificationManager();
+
+				// cancel last notification (if needed new notification will be created later)
+				nm.cancel(NOTIFICATION_ID_UNREAD_MESSAGES);
+
+				final ActivityManager.RunningTaskInfo foregroundTask = getForegroundTask();
+				if (foregroundTask == null || !foregroundTask.topActivity.getPackageName().equals(context.getPackageName())) {
+					// todo serso: make proper notification (unread messages text, small icon, etc)
+					// we are not at the top => show notification
+					final NotificationCompat.Builder nb = new NotificationCompat.Builder(context);
+					nb.setSmallIcon(R.drawable.mpp_sb_unread_messages_icon);
+					nb.setContentTitle(context.getResources().getQuantityString(R.plurals.mpp_unread_messages_count_notification, unreadMessagesCount, unreadMessagesCount));
+					nb.setContentText(context.getString(R.string.mpp_notification_text));
+					nb.setContentIntent(getActivity(context, 0, newUnreadMessagesStartIntent(context), 0));
+					nb.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+					nm.notify(NOTIFICATION_ID_UNREAD_MESSAGES, nb.getNotification());
 				}
 				break;
 		}
+	}
+
+	@Nonnull
+	private NotificationManager getNotificationManager() {
+		return (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 	}
 
 	@Nullable
